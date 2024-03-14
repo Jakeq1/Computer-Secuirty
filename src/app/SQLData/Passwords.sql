fc2f3b3ffab72b27d268efc699a66a227bd607f3
Jake <jquinn3@uccs.edu>
2024-03-13 20:00:22 -0600

    Added Email

--- src/app/SQLData/Passwords.sql
+++ src/app/SQLData/Passwords.sql
@@ -1,8 +1,6 @@
-CREATE TABLE Passwords (
+CREATE TABLE IF NOT EXISTS Passwords (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    email TEXT
 );
-
-INSERT INTO Passwords (username, password) VALUES ('user1', 'password1');
-INSERT INTO Passwords (username, password) VALUES ('user2', 'password2');
