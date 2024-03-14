ecfb203115799844ec15e3aa5ecb563338e20d6c
tristini-is <98566227+tristini-is@users.noreply.github.com>
2024-03-13 19:56:10 -0600

    Basic functionality
    Created basic login and registration pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent},
+];
